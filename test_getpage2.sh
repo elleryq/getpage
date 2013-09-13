3b4fbd61ba187082d00a6b83388fe122ef1a71aa
elleryq <elleryq@gmail.com>
2013-09-13 17:17:46 +0800

    Add -O option to specify output directory.
     * Make -o works
     * Show url when 404 happened.

--- test_getpage2.sh
+++ test_getpage2.sh
@@ -2,3 +2,5 @@
 export http_proxy=http://localhost:3128
 python src/getpage2 http://t.co/Td4WepML4B
 python src/getpage2 http://www.icoding.co/2012/08/logging-html
+python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html
+python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O output/2013
