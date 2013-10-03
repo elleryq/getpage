e54387fb56d2b224626e6d526bf2ea36cc448716
elleryq <elleryq@gmail.com>
2013-10-03 16:03:25 +0800

    Use logging.
     * Still debug logging.

--- test_getpage2.sh
+++ test_getpage2.sh
@@ -4,3 +4,4 @@ python src/getpage2 http://t.co/Td4WepML4B
 python src/getpage2 http://www.icoding.co/2012/08/logging-html
 python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html
 python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013
+python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013 -q
