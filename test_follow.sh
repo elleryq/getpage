30b5097c8d73de6bbc414da18a6ed979dd92ad5d
elleryq <elleryq@gmail.com>
2013-10-18 22:07:25 +0800

    Modify src to scripts.

--- test_follow.sh
+++ test_follow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export http_proxy=http://localhost:3128
-python src/getpage http://t.co/Td4WepML4B
+python scripts/getpage http://t.co/Td4WepML4B
--- test_getpage2.sh
+++ test_getpage2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export http_proxy=http://localhost:3128
-python src/getpage2 http://t.co/Td4WepML4B
-python src/getpage2 http://www.icoding.co/2012/08/logging-html
-python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html
-python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013
-python src/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013 -q
+python scripts/getpage2 http://t.co/Td4WepML4B
+python scripts/getpage2 http://www.icoding.co/2012/08/logging-html
+python scripts/getpage2 http://t.co/g4h2QXVT3z -o aa.html
+python scripts/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013
+python scripts/getpage2 http://t.co/g4h2QXVT3z -o aa.html -O test/2013 -q
