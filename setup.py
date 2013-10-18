552b62df99b8a0c0628f24bcbb138a270758ef19
elleryq <elleryq@gmail.com>
2013-10-18 22:26:48 +0800

    Add setup.py/MANIFEST.in for installation.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 
 setup(name='getpage2',
-      version='1.0',
-      scripts=['scripts/getpage', 'scripts/getpage2']
+      version='0.1',
+      description='getpage2: grab HTML, transformed by readability and save',
+      author='Yan-ren Tsai',
+      author_email='elleryq@gmail.com',
+      url='https://github.com/elleryq/getpage',
+      scripts=['scripts/getpage', 'scripts/getpage2'],
+      data_files=[]
       )
